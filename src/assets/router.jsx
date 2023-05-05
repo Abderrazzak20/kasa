e9f961f346236a2c817558c993c8981cdc8b11a4
bourmou <abdirrazzak44@gmail.com>
2023-05-05 19:44:27 +0200

    complet

--- src/assets/router.jsx
+++ src/assets/router.jsx
@@ -39,8 +39,7 @@ export const router = createBrowserRouter([
         element: <About />
       },
       {
-        path: "/*/",
-        
+        path: "/kasa/*",
         element: <PageNotFound />
       }
     ]

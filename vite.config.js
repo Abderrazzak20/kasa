c5351b178e4b45cf1dd1012cc6a48f1bb20bb686
bourmou <abdirrazzak44@gmail.com>
2023-05-05 21:19:25 +0200

    complet

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Home from './src/Pages/Home'
+import ApartementsPage from './src/Pages/ApartementsPage'
+import About from './src/Pages/About'
+import PageNotFound from './src/Pages/PageNotFound'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/kasa/",
   plugins: [react()],
+  routes: [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/:id',
+      component: ApartementsPage,
+    },
+    {
+      path: '/about',
+      component: About,
+    },
+    {
+      path: '*',
+      component: PageNotFound,
+    },
+  ],
 })

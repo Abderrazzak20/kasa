e8751ef04153e5ce7a59fc712ec335021d740ded
bourmou <abdirrazzak44@gmail.com>
2023-05-05 21:13:30 +0200

    complet

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { name } from './package.json'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${name}/`,
+  base: "/kasa/",
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
 })
